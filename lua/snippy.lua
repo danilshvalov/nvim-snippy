dc8341558fe4d624b71a8fc587d5aa39a45f9dc8
Daniil Shvalov <shvalovich@gmail.com>
2022-01-07 01:33:21 +0300

    try fix

--- lua/snippy.lua
+++ lua/snippy.lua
@@ -180,9 +180,9 @@ end
 -- Autocmd handlers
 
 function M._handle_TextChanged()
-    M._check_position()
     buf.fix_current_stop()
     buf.update_state()
+    M._check_position()
     M._mirror_stops()
 end
 
